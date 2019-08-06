41daef7d3a3968cc33e2639913096e286bd1eb03
KevTLW <kevinmcruzc@gmail.com>
2019-08-06 18:55:27 -0400

    Included code to serve static files in production and to host with Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
+const path = require('path');
 const connectToMongoDB = require('./config/db');
 const connectToPassport = require('./config/passport');
 
@@ -12,6 +13,14 @@ app.use(passport.initialize());
 connectToPassport(passport);
 
 app.use('/api/users', require('./routes/users'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Connected to server on port ${port}`));
 connectToMongoDB();
