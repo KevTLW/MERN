e1e07ea2ea5deb2d897881d2784a344ebe1f7241
KevTLW <kevinmcruzc@gmail.com>
2019-08-06 14:51:30 -0400

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const connectToMongoDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Connected to server on port ${port}`));
+connectToMongoDB();
